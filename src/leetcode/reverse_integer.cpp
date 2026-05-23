974c84d39e2a0de0b263ef6d265d573d00e4fc29
brindharamani08 <brindha0819ramani@gmail.com>
2026-05-23 13:06:52 +0530

    Update reverse_integer.cpp

--- src/leetcode/reverse_integer.cpp
+++ src/leetcode/reverse_integer.cpp
@@ -4,7 +4,8 @@ public:
         int reverse=0;
         while(x!=0){
             int last=x%10;
-            if(reverse<INT_MAX/10||reverse>INT_MIN/10)
+            if(reverse>INT_MAX/10||reverse<INT_MIN/10)
+                return 0;
             reverse=reverse*10+last;
             x=x/10;
 
